6fe55bb67b3df07401514f101a98acb7a57fe999
Denis Burmak <49818484+den258258@users.noreply.github.com>
2026-07-07 21:45:45 +0300

    Update server.js

--- sheiker/server.js
+++ sheiker/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+
 const app = express();
 app.use(express.json());
 app.use(express.static(__dirname)); // отдаёт index.html, style.css, script.js
